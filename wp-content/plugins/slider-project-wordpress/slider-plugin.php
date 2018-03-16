bb6c752c60b6ee344b78e515ae8ce184cec1c005
Backins <31820492+Backins@users.noreply.github.com>
2018-03-16 13:54:13 +0100

    Update slider-plugin.php

--- wp-content/plugins/slider-project-wordpress/slider-plugin.php
+++ wp-content/plugins/slider-project-wordpress/slider-plugin.php
@@ -19,30 +19,6 @@ function slider_display()
 class sliderProjectWordPress {
     public function __construct() {
         include_once plugin_dir_path(__FILE__).'/slider.php';
-        register_activation_hook(__FILE__, array('sliderProjectWordPress', 'install'));
-        register_uninstall_hook(__FILE__, array('sliderProjectWordPress', 'uninstall'));
-        add_action('wp_loaded', array($this, 'save_email'));
-    }
-
-    public static function install() {
-        global $wpdb;
-        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ma_newsletter_list (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL);");
-    }
-
-    public static function uninstall() {
-        global $wpdb;
-        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}ma_newsletter_list;");
-    }
-
-    public function save_email() {
-        if (isset($_POST['ma_newsletter_email']) && !empty($_POST['ma_newsletter_email'])) {
-            global $wpdb;
-            $email = $_POST['ma_newsletter_email'];
-            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}ma_newsletter_list WHERE email = '$email'");
-            if (is_null($row)) {
-                $wpdb->insert("{$wpdb->prefix}ma_newsletter_list", array('email' => $email));
-            }
-        }
     }
 
 }
